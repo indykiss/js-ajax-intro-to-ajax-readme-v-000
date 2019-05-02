cbefb6c089fe82c8c8d57df4f416e943aabcea65
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-02 18:30:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,7 +22,13 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
 
 function showCommits() {
   const commits = JSON.parse(this.responseText);
