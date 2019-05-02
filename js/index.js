aab484869cbda3618d73f478f93a72d135000dae
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-02 18:30:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,3 +21,20 @@ function showRepositories() {
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+
+
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
